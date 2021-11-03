576e6277814d5bbd4f42cc82701999e584a824aa
FoLZer <nikolaenko.gleb.044@gmail.com>
2021-11-04 02:49:11 +0300

    Test update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const edge = require("edge-js");
 
 const Calc = edge.func({
-    assemblyFile: 'osu_Server_DifficultyCalculator.dll'
+    assemblyFile: 'osu_Server_DifficultyCalculator.dll',
+    typeName: 'osu.Server.DifficultyCalculator.Startup',
+    methodName: 'Invoke'
 });
 
 module.exports = {Calc};
